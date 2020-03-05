7b8f32585a090ed6909915ba94b86ecfcb76d377
catrinanguyen <c1nguyen@scu.edu>
2020-03-04 22:53:04 -0800

    explore screen test

--- main-app/screens/explore/Explore.js
+++ main-app/screens/explore/Explore.js
@@ -1,28 +1,61 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import {StyleSheet, Button, Text, View, TouchableOpacity, Image,} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create ({
-  container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-  }
+    header: {
+        fontSize: 50,
+        paddingTop: 10,
+        paddingBottom: 20
+    },
+    buttonContainer:{
+        flexDirection: 'row', borderWidth: 1, paddingVertical: 10
+    },
+    lastButtonContainer:{
+        flexDirection: 'row', borderBottomWidth: 1, paddingVertical: 10
+    },
+    buttonLeft:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    buttonRight:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    }
 });
+
 
 class Explore extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Explore Screen</Text>
-        <Button
-          title="Know Before You Go"
-          onPress={() => this.props.navigation.navigate("KnowBefore")}
-        />
-          <Button
-              title="Plan Your Trip"
-              onPress={() => this.props.navigation.navigate("Plan")}
-          />
+      <View>
+        <Text style={styles.header}>Explore</Text>
+        <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => this.props.navigation.navigate("KnowBefore")}>
+            <View style={styles.buttonLeft}>
+                <Ionicons name="ios-book" size={32} color="green" />
+                <Text style={{fontWeight: "bold"}}>Know Before You Go</Text>
+            </View>
+            <View style={styles.buttonRight}>
+                <Ionicons name="ios-book" size={32} color="green" />
+            </View>
+        </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.lastButtonContainer}
+              onPress={() => this.props.navigation.navigate("Plan")}>
+              <View style={styles.buttonLeft}>
+                  <Ionicons name="ios-book" size={32} color="green" />
+                  <Text style={{fontWeight: "bold"}}>Plan Your Trip</Text>
+              </View>
+              <View style={styles.buttonRight}>
+                 <Ionicons name="ios-book" size={32} color="green" />
+              </View>
+          </TouchableOpacity>
       </View>
     );
   }
--- main-app/screens/explore/plan/activities/Fishing.js
+++ main-app/screens/explore/plan/activities/Fishing.js
@@ -18,6 +18,7 @@ class Fishing extends React.Component {
           <ScrollView>
               <Text>Sport fishing is prohibited in Galapagos until it can be carefully determined that it can be done sustainably. However, one of the possibilities being explored is known as "Experiential Fishing" or demonstration fishing. The artisanal fishermen of Galapagos will be able to offer the experience fishing service to the tourists of the archipelago. This combines traditional skills with tourism, and using “experiential fishing” can help fishermen's families, whose income is affected by the consequences of overfishing and also by the Special Law, maintain their livelihoods.</Text>
           </ScrollView>
+
       </View>
     );
   }
