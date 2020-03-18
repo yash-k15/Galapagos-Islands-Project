b3580690e3f0e792d3edac355900b29252ba1ae6
Mariah Manzano <mmanzano@scu.edu>
2020-03-17 20:04:02 -1000

    updated know before and learn pages

--- main-app/screens/explore/know/KnowBefore.js
+++ main-app/screens/explore/know/KnowBefore.js
@@ -1,49 +1,134 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {
-  StyleSheet,
-  Button,
-  Text,
-  View,
-  ScrollView,
-} from 'react-native'
+import {StyleSheet, Button, Text, View, TouchableOpacity, Image, Dimensions,} from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+
+//get scaling factors
+const entireScreenWidth = Dimensions.get('window').width;
+let rem;
+rem = entireScreenWidth / 350;
 
 const styles = StyleSheet.create ({
-  container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-  }
+    header:{
+        flexDirection: 'row',
+        paddingTop: 16,
+        paddingBottom: 16,
+        paddingLeft: 34,
+        alignItems: 'center'
+    },
+    headerText: {
+        paddingLeft: 16,
+        fontSize: 28*rem,
+        fontWeight: "600"
+    },
+    buttonText:{
+        fontSize: 17*rem,
+        paddingLeft: 16
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderColor: '#C0C0C0',
+        paddingVertical: 10,
+        paddingLeft: 34,
+        paddingRight: 14
+    },
+    lastButtonContainer:{
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+        borderColor: '#C0C0C0',
+        paddingLeft: 34,
+        paddingRight: 14
+    },
+    buttonLeft:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    buttonRight:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    }, 
+    chevron:{
+        width: 11*rem, 
+        height: 18*rem
+    }
 });
 
 class KnowBefore extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
-          <ScrollView>
-            <Text>Explore Screen</Text>
-            <Button
-              title="Language"
-              onPress={() => this.props.navigation.navigate("Language")}
-            />
-              <Button
-                  title="Cultural & Social Customs"
-                  onPress={() => this.props.navigation.navigate("Customs")}
-              />
-              <Button
-                  title="Rules of the National Park"
-                  onPress={() => this.props.navigation.navigate("Rules")}
-              />
-              <Text>Internet & Connectivity</Text>
+      <View style={{backgroundColor: 'white', flex: 1}}>
+          <View style={styles.header}>
+                <Image
+                    source={require('../../../app/assets/icons/plan.png')}
+                    style={{width: 28*rem, height: 31*rem}}
+                />
+              <Text style={styles.headerText}>Know Before You Go</Text>
+          </View>
+          <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={() => this.props.navigation.navigate("Language")}>
+              <View style={styles.buttonLeft}>
+                    <Image
+                        source={require('../../../app/assets/icons/topActivities_gray.png')}
+                        style={{width: 30*rem, height: 30*rem}}
+                    />                  
+                    <Text style={styles.buttonText}>Language</Text>
+              </View>
+              <View style={styles.buttonRight}>
+                    <Image
+                        source={require('../../../app/assets/icons/chevron.png')}
+                        style={styles.chevron}
+                    />                
+                </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={() => this.props.navigation.navigate("Customs")}>
+              <View style={styles.buttonLeft}>
+                    <Image
+                        source={require('../../../app/assets/icons/island_gray.png')}
+                        style={{width: 27*rem, height: 31*rem}}
+                    />                   
+                    <Text style={styles.buttonText}>Cultural & Social Norms</Text>
+              </View>
+              <View style={styles.buttonRight}>
+                    <Image
+                        source={require('../../../app/assets/icons/chevron.png')}
+                        style={styles.chevron}
+                    />                   
+                </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={() => this.props.navigation.navigate("Rules")}>
+              <View style={styles.buttonLeft}>
+                    <Image
+                        source={require('../../../app/assets/icons/island_gray.png')}
+                        style={{width: 27*rem, height: 31*rem}}
+                    />                   
+                    <Text style={styles.buttonText}>Rules of the National Park</Text>
+              </View>
+              <View style={styles.buttonRight}>
+                    <Image
+                        source={require('../../../app/assets/icons/chevron.png')}
+                        style={styles.chevron}
+                    />                   
+                </View>
+          </TouchableOpacity>
+          <Text>Internet & Connectivity</Text>
               <Text>Internet connection can be obtained in all places of accommodation / restaurants. The internet in
                   Puerto Ayora is satellite, therefore we do not have a fiber optic connection which can make it
                   slow.</Text>
               <Text>Have an Emergency?</Text>
               <Text>911</Text>
-          </ScrollView>
       </View>
     );
   }
 }
-
 export default KnowBefore;
--- main-app/screens/learn/Learn.js
+++ main-app/screens/learn/Learn.js
@@ -1,35 +1,122 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import {StyleSheet, Button, Text, View, TouchableOpacity, Image, Dimensions,} from 'react-native';
+
+//get scaling factors
+const entireScreenWidth = Dimensions.get('window').width;
+let rem;
+rem = entireScreenWidth / 350;
 
 const styles = StyleSheet.create ({
-    title: {
-        textAlign:'left'
+    header: {
+        fontSize: 34*rem,
+        fontWeight: "600",
+        paddingVertical: 16,
+        paddingLeft: 34,
+        paddingRight: 59
     },
-  container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-  }
+    buttonText:{
+        fontSize: 17*rem,
+        paddingLeft: 16
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: '#C0C0C0',
+        paddingVertical: 10,
+        paddingLeft: 34,
+        paddingRight: 14
+    },
+    lastButtonContainer:{
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+        borderColor: '#C0C0C0',
+        paddingLeft: 34,
+        paddingRight: 14
+    },
+    buttonLeft:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    buttonRight:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    chevron:{
+        width: 11*rem, 
+        height: 18*rem
+    }
 });
+
 
 class Learn extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Learn Screen</Text>
-        <Button
-          title="About Us"
-          onPress={() => this.props.navigation.navigate("AboutUs")}
+      <View style={{backgroundColor: 'white', flex: 1}}>
+        <Text style={styles.header}>Learn</Text>
+
+        <Image
+            source={require('../../app/assets/images/headerImage_short.png')}
+            style={{width: entireScreenWidth, height: 25*rem}}
         />
-          <Button
-              title="Science & Sustainability"
-              onPress={() => this.props.navigation.navigate("ScienceAndSus")}
-          />
-          <Button
-              title="History"
-              onPress={() => this.props.navigation.navigate("History")}
-          />
+
+        <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => this.props.navigation.navigate("AboutUs")}>
+            <View style={styles.buttonLeft}>
+                <Image
+                    source={require('../../app/assets/icons/knowBefore_gray.png')}
+                    style={{width: 28*rem, height: 28*rem}}
+                />
+                <Text style={styles.buttonText}>About Us</Text>
+            </View>
+            <View style={styles.buttonRight}>
+                <Image
+                    source={require('../../app/assets/icons/chevron.png')}
+                    style={styles.chevron}
+                />
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={styles.lastButtonContainer}
+            onPress={() => this.props.navigation.navigate("ScienceAndSus")}>
+            <View style={styles.buttonLeft}>
+                <Image
+                      source={require('../../app/assets/icons/plan_gray.png')}
+                      style={{width: 28*rem, height: 31*rem}}
+                />                  
+                <Text style={styles.buttonText}>Science & Sustainability</Text>
+              </View>
+              <View style={styles.buttonRight}>
+                    <Image
+                        source={require('../../app/assets/icons/chevron.png')}
+                        style={styles.chevron}
+                    />              
+              </View>
+              <View style={{borderBottomWidth: 1}}></View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.lastButtonContainer}
+            onPress={() => this.props.navigation.navigate("History")}>
+            <View style={styles.buttonLeft}>
+                <Image
+                      source={require('../../app/assets/icons/plan_gray.png')}
+                      style={{width: 28*rem, height: 31*rem}}
+                />                  
+                <Text style={styles.buttonText}>History</Text>
+              </View>
+              <View style={styles.buttonRight}>
+                    <Image
+                        source={require('../../app/assets/icons/chevron.png')}
+                        style={styles.chevron}
+                    />              
+              </View>
+              <View style={{borderBottomWidth: 1}}></View>
+          </TouchableOpacity>
       </View>
     );
   }
