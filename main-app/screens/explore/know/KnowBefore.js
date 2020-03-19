0dc20946b21b279faf4545f114947386d4a4e177
Mariah Manzano <mmanzano@scu.edu>
2020-03-18 20:19:46 -1000

    updated explore pages

--- main-app/screens/explore/know/KnowBefore.js
+++ main-app/screens/explore/know/KnowBefore.js
@@ -56,6 +56,20 @@ const styles = StyleSheet.create ({
     chevron:{
         width: 11*rem, 
         height: 18*rem
+    },
+    regularBold:{
+        fontWeight: '600',
+        color: '#000000',
+        fontSize: 17*rem,
+        paddingLeft: 34,
+        paddingTop: 16, 
+        paddingBottom: 16
+    },
+    regular:{
+      fontSize: 17*rem,
+      color: '#616161',
+      paddingLeft: 34,
+      lineHeight: 20 * rem
     }
 });
 
@@ -63,71 +77,91 @@ class KnowBefore extends React.Component {
   render(){
     return (
       <View style={{backgroundColor: 'white', flex: 1}}>
-          <View style={styles.header}>
+        <View style={styles.header}>
+            <Image
+                source={require('../../../app/assets/icons/plan.png')}
+                style={{width: 28*rem, height: 31*rem}}
+            />
+            <Text style={styles.headerText}>Know Before You Go</Text>
+        </View>
+        <Image
+            source={require('../../../app/assets/images/headerImage_short.png')}
+            style={{width: entireScreenWidth, height: 25*rem}}
+        />
+        <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => this.props.navigation.navigate("Language")}>
+            <View style={styles.buttonLeft}>
                 <Image
-                    source={require('../../../app/assets/icons/plan.png')}
-                    style={{width: 28*rem, height: 31*rem}}
-                />
-              <Text style={styles.headerText}>Know Before You Go</Text>
-          </View>
-          <TouchableOpacity
-              style={styles.buttonContainer}
-              onPress={() => this.props.navigation.navigate("Language")}>
-              <View style={styles.buttonLeft}>
-                    <Image
-                        source={require('../../../app/assets/icons/topActivities_gray.png')}
-                        style={{width: 30*rem, height: 30*rem}}
-                    />                  
-                    <Text style={styles.buttonText}>Language</Text>
-              </View>
-              <View style={styles.buttonRight}>
-                    <Image
-                        source={require('../../../app/assets/icons/chevron.png')}
-                        style={styles.chevron}
-                    />                
-                </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-              style={styles.buttonContainer}
-              onPress={() => this.props.navigation.navigate("Customs")}>
-              <View style={styles.buttonLeft}>
-                    <Image
-                        source={require('../../../app/assets/icons/island_gray.png')}
-                        style={{width: 27*rem, height: 31*rem}}
-                    />                   
-                    <Text style={styles.buttonText}>Cultural & Social Norms</Text>
-              </View>
-              <View style={styles.buttonRight}>
-                    <Image
-                        source={require('../../../app/assets/icons/chevron.png')}
-                        style={styles.chevron}
-                    />                   
-                </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-              style={styles.buttonContainer}
-              onPress={() => this.props.navigation.navigate("Rules")}>
-              <View style={styles.buttonLeft}>
-                    <Image
-                        source={require('../../../app/assets/icons/island_gray.png')}
-                        style={{width: 27*rem, height: 31*rem}}
-                    />                   
-                    <Text style={styles.buttonText}>Rules of the National Park</Text>
-              </View>
-              <View style={styles.buttonRight}>
-                    <Image
-                        source={require('../../../app/assets/icons/chevron.png')}
-                        style={styles.chevron}
-                    />                   
-                </View>
-          </TouchableOpacity>
-          <Text>Internet & Connectivity</Text>
-              <Text>Internet connection can be obtained in all places of accommodation / restaurants. The internet in
+                    source={require('../../../app/assets/icons/topActivities_gray.png')}
+                    style={{width: 30*rem, height: 30*rem}}
+                />                  
+                <Text style={styles.buttonText}>Language</Text>
+            </View>
+            <View style={styles.buttonRight}>
+                <Image
+                    source={require('../../../app/assets/icons/chevron.png')}
+                    style={styles.chevron}
+                />                
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => this.props.navigation.navigate("Customs")}>
+            <View style={styles.buttonLeft}>
+                <Image
+                    source={require('../../../app/assets/icons/island_gray.png')}
+                    style={{width: 27*rem, height: 31*rem}}
+                />                   
+                <Text style={styles.buttonText}>Cultural & Social Norms</Text>
+            </View>
+            <View style={styles.buttonRight}>
+                <Image
+                    source={require('../../../app/assets/icons/chevron.png')}
+                    style={styles.chevron}
+                />                   
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={styles.lastButtonContainer}
+            onPress={() => this.props.navigation.navigate("Rules")}>
+            <View style={styles.buttonLeft}>
+                <Image
+                    source={require('../../../app/assets/icons/island_gray.png')}
+                    style={{width: 27*rem, height: 31*rem}}
+                />                   
+                <Text style={styles.buttonText}>Rules of the National Park</Text>
+            </View>
+            <View style={styles.buttonRight}>
+                <Image
+                    source={require('../../../app/assets/icons/chevron.png')}
+                    style={styles.chevron}
+                />                   
+            </View>
+        </TouchableOpacity>
+        <Text style={styles.regularBold}>Internet & Connectivity</Text>
+        <Text style={styles.regular}>Internet connection can be obtained in all places of accommodation / restaurants. The internet in
                   Puerto Ayora is satellite, therefore we do not have a fiber optic connection which can make it
                   slow.</Text>
-              <Text>Have an Emergency?</Text>
-              <Text>911</Text>
-      </View>
+        <Text style={styles.regularBold}>Have an Emergency?</Text>
+        <TouchableOpacity
+            style={styles.lastButtonContainer}
+            onPress={() => this.props.navigation.navigate("Rules")}>
+            <View style={styles.buttonLeft}>
+                <Image
+                    source={require('../../../app/assets/icons/phone.png')}
+                    style={{width: 25*rem, height: 25*rem}}
+                />                   
+                <Text style={styles.buttonText}>Call 911</Text>
+            </View>
+            <View style={styles.buttonRight}>
+                <Image
+                    source={require('../../../app/assets/icons/chevron.png')}
+                    style={styles.chevron}
+                />                   
+            </View>
+        </TouchableOpacity>
+    </View>
     );
   }
 }
