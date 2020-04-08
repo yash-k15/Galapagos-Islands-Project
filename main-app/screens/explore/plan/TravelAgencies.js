c8abb8ed73802bcd6f8d80d08c9ebd55cb471419
Mariah Manzano <mmanzano@scu.edu>
2020-04-08 09:58:55 -0700

    travel agencies update

--- main-app/screens/explore/plan/TravelAgencies.js
+++ main-app/screens/explore/plan/TravelAgencies.js
@@ -1,26 +1,102 @@
 import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { StyleSheet, Button, Text, View, ScrollView } from 'react-native';
+import {StyleSheet, Button, Text, View, TouchableOpacity, Image, Dimensions, SafeAreaView, ScrollView,} from 'react-native';
+
+//get scaling factors
+const entireScreenWidth = Dimensions.get('window').width;
+let rem;
+rem = entireScreenWidth / 350;
 
 const styles = StyleSheet.create ({
-  container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-  }
+    header:{
+        flexDirection: 'row',
+        paddingTop: 16,
+        paddingBottom: 16,
+        paddingLeft: 34,
+        alignItems: 'center'
+    },
+    headerText: {
+        paddingLeft: 16,
+        fontSize: 28*rem,
+        fontWeight: "600"
+    },
+    buttonText:{
+        fontSize: 17*rem,
+        paddingLeft: 16
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderColor: '#C0C0C0',
+        paddingVertical: 10,
+        paddingLeft: 34,
+        paddingRight: 14
+    },
+    lastButtonContainer:{
+        flexDirection: 'row',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+        borderColor: '#C0C0C0',
+        paddingLeft: 34,
+        paddingRight: 14
+    },
+    buttonLeft:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    buttonRight:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    }, 
+    chevron:{
+        width: 11*rem, 
+        height: 18*rem
+    },
+    regularBold:{
+        fontWeight: '600',
+        color: '#000000',
+        fontSize: 17*rem,
+        paddingLeft: 34,
+        paddingTop: 16, 
+        paddingBottom: 16
+    },
+    regular:{
+      fontSize: 17*rem,
+      color: '#616161',
+      paddingLeft: 34,
+      paddingRight: 65,
+      lineHeight: 20 * rem
+    },
+    numberRow:{
+      flexDirection: 'row',
+      paddingTop: 16,
+      paddingBottom: 16,
+      paddingLeft: 34,
+      alignItems: 'stretch'
+    }
 });
 
 class TravelAgencies extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      <View style={{backgroundColor: 'white', flex: 1}}>
           <Text>Travel Agencies</Text>
           <ScrollView>
             <Text>There are several places where you can camp with prior written authorization from the PNG Directorate, in Santa Cruz, San Cristóbal and Isabela.</Text>
           </ScrollView>
       </View>
+      </ScrollView>
+      </SafeAreaView>
     );
   }
 }
+
 
 export default TravelAgencies;
